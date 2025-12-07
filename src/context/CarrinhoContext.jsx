84f6fbeb2ea0fb04f780dfa7a67ec2318d9c5162
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-12-07 12:36:17 -0300

    Atualização do projeto

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,21 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = "Carrinho"
 
+const estadoInicial = [];
+
 export const CarrinhoProvider = ({children}) => {
-    const [carrinho, setCarrinho] = useState([]);
+    const [carrinho, dispach] = useReducer(carrinhoReducer, estadoInicial)
     const [quantidade, setQuantidade] = useState(0);
-    const [valorTotal, setValorTotal] = useState(0)
+    const [valorTotal, setValorTotal] = useState(0);
 
     return (
         <CarrinhoContext.Provider value={{ 
             carrinho, 
-            setCarrinho, 
+            dispach,
             quantidade, 
-            setQuantidade,
             valorTotal, 
-            setValorTotal 
             }}>
             {children}
         </CarrinhoContext.Provider>
